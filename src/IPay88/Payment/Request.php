2c393f3a8aabf49bbaef2bab3bd06855ebf2fd17
Fikri <fikri.marhan@gmail.com>
2014-04-18 15:49:29 +0800

    Clean up stuff

--- src/IPay88/Payment/Request.php
+++ src/IPay88/Payment/Request.php
@@ -1,7 +1,10 @@
 <?php namespace IPay88\Payment;
 
+use IPay88\Security\Signature;
+
 class Request
 {
+	private $merchantKey;
 	private $merchantCode;
 	private $paymentId;
 	private $currency;
@@ -14,9 +17,16 @@ class Request
 	private $remark;
 	private $status;
 
+    public static $requeryUrl = 'https://www.mobile88.com/epayment/enquiry.asp';
+
 	public function getMerchantCode()
 	{
 		return $this->merchantCode;
+	}
+
+	public function setMerchantCode($val)
+	{
+		return $this->merchantCode = $val;
 	}
 
 	public function getPaymentId()
@@ -24,9 +34,19 @@ class Request
 		return $this->paymentId;
 	}
 
+	public function setPaymentId($val)
+	{
+		return $this->paymentId = $val;
+	}
+
 	public function getCurrency()
 	{
 		return $this->currency;
+	}
+
+	public function setCurrency($val)
+	{
+		return $this->currency = $val;
 	}
 
 	public function getTransId()
@@ -34,9 +54,19 @@ class Request
 		return $this->transId;
 	}
 
+	public function setTransId($val)
+	{
+		return $this->transId = $val;
+	}
+
 	public function getAuthCode()
 	{
 		return $this->authCode;
+	}
+
+	public function setAuthCode($val)
+	{
+		return $this->authCode = $val
 	}
 
 	public function getErrDesc()
@@ -44,9 +74,20 @@ class Request
 		return $this->errDesc;
 	}
 
+	public function setErrDesc($val)
+	{
+		return $this->errDesc = $val;
+	}
+
 	public function getSignature()
 	{
-		return $this->signature;
+		return Signature::generate_signature(
+			$this->merchantKey,
+			$this->getMerchantCode(),
+			$this->getRefNo(),
+			$this->getAmount(),
+			$this->getCurrency()
+			);
 	}
 
 	public function getRefNo()
@@ -54,9 +95,19 @@ class Request
 		return $this->refNo;
 	}
 
+	public function setRefNo($val)
+	{
+		return $this->refNo = $val;
+	}
+
 	public function getAmount()
 	{
 		return $this->amount;
+	}
+
+	public function setAmount($val)
+	{
+		return $this->amount = $val;
 	}
 
 	public function getRemark()
@@ -64,12 +115,21 @@ class Request
 		return $this->remark;
 	}
 
+	public function setRemark($val)
+	{
+		return $this->remark = $val;
+	}
+
 	public function getStatus()
 	{
 		return $this->status;
 	}
 
-    public static $requeryUrl = 'https://www.mobile88.com/epayment/enquiry.asp';
+	public function setStatus($val)
+	{
+		return $this->status = $val;
+	}
+
 
      /**
      * Check payment status (re-query).
@@ -87,16 +147,19 @@ class Request
      * - Payment fail       - Payment failed.
      * - M88Admin           - Payment status updated by Mobile88 Admin (Fail)
      */
-    public static function requery($merchantCode, $refNo, $amount) {
+    public static function requery($merchantCode, $refNo, $amount) 
+    {
         if (!function_exists('curl_init')) {
             trigger_error('PHP cURL extension is required.');
             return false;
         }
-        $curl = curl_init(self::$requeryUrl . '?' . http_build_query(array(
-        	'MerchantCode' => $merchantCode,
-        	'RefNo' => $refNo,
-        	'Amount'=> $amount
-        	)));
+        $curl = curl_init(
+        			self::$requeryUrl . '?' . http_build_query(array(
+		        		'MerchantCode' => $merchantCode,
+		        		'RefNo' => $refNo,
+		        		'Amount'=> $amount
+		        	))
+		        );
 
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $result = trim(curl_exec($curl));
@@ -105,9 +168,8 @@ class Request
         return $result;
     }
 
-
-    public function __construct($argument1)
+    public function __construct($merchantKey)
     {
-        // TODO: write logic here
+    	$this->merchantKey = $merchantKey;
     }
 }
